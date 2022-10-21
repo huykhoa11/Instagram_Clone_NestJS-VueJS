efc576754ee1ecb449b647d68b7a2441609e5637
Tran Huy Khoal.com <huykhoa11@gmail.com>
2022-10-22 01:37:15 +0900

    fix bugs

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  staticAssetsDir: '/src/uploads'
+  // staticAssetsDir: '/src/uploads'
 })
