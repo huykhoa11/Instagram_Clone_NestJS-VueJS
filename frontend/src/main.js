12ad8d659e29f3095d20f10991bd2428916a0f88
Tran Huy Khoal.com <huykhoa11@gmail.com>
2023-02-25 16:46:00 +0900

    create branch backend

--- frontend/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router/router'
-import {store} from './store.js'
-import { i18n } from './langs/languages'
-
-createApp(App).use(i18n).use(store).use(router).mount('#app')
